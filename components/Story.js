163bd35bea74430ebc7b863320111f0aee228a94
abha <abha@example.com>
2023-03-15 21:08:25 +0530

    stiry section added and styled

--- components/Story.js
+++ components/Story.js
@@ -3,8 +3,13 @@ import React from "react";
 const Story = ({ img, username }) => {
   return (
     <div>
-      <img src={img} alt={username} />
-      <p>{username}</p>
+      <img
+        className="h-14 rounded-full p-[1.5px] border-red-500 cursor-pointer hover:scale-110
+        transition-transform duration-200"
+        src={img}
+        alt={username}
+      />
+      <p className="text-xs w-14 truncate">{username}</p>
     </div>
   );
 };
