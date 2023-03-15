dfbffa38dcae430a8107fb4692baf6ab1b8393a0
abha <abha@example.com>
2023-03-15 17:09:10 +0530

    populate fake data using minifaker

---
+++ components/Feed.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Stories from "./Stories";
+
+const Feed = () => {
+  return (
+    <main>
+      <section>
+        {/*stories*/}
+        <Stories />
+        {/*posts*/}
+      </section>
+
+      <section>
+        {/*mini profile*/}
+        {/*suggestions*/}
+      </section>
+    </main>
+  );
+};
+
+export default Feed;
---
+++ components/Story.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Story = ({ img, username }) => {
+  return (
+    <div>
+      <img src={img} alt={username} />
+      <p>{username}</p>
+    </div>
+  );
+};
+
+export default Story;
